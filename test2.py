9dfa814d87f56e0fbe0eb7f9bebb8c2f64c6835f
Solefelicee <solefelicee@yandex.ru>
2024-04-04 23:22:23 +0300

    error

--- test2.py
+++ test2.py
@@ -20,11 +20,11 @@ def test_syntax_of_another_file():
     file_path = '2.py'  # Путь к вашему файлу с кодом
 
     # Проверяем, существует ли файл
-    assert os.path.exists(file_path), f"Файл {file_path} не найден"
+    assert os.path.exists(file_path), f"Файл {file_path} не найден
 
     try:
         # Читаем содержимое файла с явным указанием кодировки UTF-8
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, 'r, encoding='utf-8') as file:
             code = file.read()
 
         # Пытаемся скомпилировать код
